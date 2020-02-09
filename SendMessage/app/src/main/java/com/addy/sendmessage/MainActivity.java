e0a559c31ffe3916a7383a59e4a233b100ace257
Aditya Raj <aditya.x510@gmail.com>
2020-02-09 12:46:59 +0530

    init

--- SendMessage/app/src/main/java/com/addy/sendmessage/MainActivity.java
+++ SendMessage/app/src/main/java/com/addy/sendmessage/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        test
 
         num = findViewById(R.id.editText2);
         msg = findViewById(R.id.editText3);

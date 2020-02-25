fe19c34bfe5235b8ed892c8ed21ee5db0a08d5fc
Aditya Raj <aditya.x510@gmail.com>
2020-02-25 19:15:49 +0530

    init

---
+++ CustomToast/app/src/main/java/com/addy/customtoast/MainActivity.java
@@ -0,0 +1,44 @@
+package com.addy.customtoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.zip.Inflater;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btnToast);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.activity_toast,(ViewGroup)findViewById(R.id.linearToastCustom));
+                ImageView imageView = layout.findViewById(R.id.custom_toast_image);
+                imageView.setImageResource(R.drawable.test);
+                Toast toast = new Toast(getApplicationContext());
+                toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(layout);
+                toast.show();
+            }
+        });
+
+
+
+
+    }
+}

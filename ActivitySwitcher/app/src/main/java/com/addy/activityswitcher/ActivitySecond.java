15c2e3d7c5c95bcf3bfef1ce501f8851570ec3f0
Aditya Raj <aditya.x510@gmail.com>
2020-03-04 22:25:51 +0530

    init

--- ActivitySwitcher/app/src/main/java/com/addy/activityswitcher/ActivitySecond.java
+++ ActivitySwitcher/app/src/main/java/com/addy/activityswitcher/ActivitySecond.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class ActivitySecond extends AppCompatActivity {
     ImageButton btn1, btn2;
+    TextView tv;
+    EditText res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,19 +20,27 @@ public class ActivitySecond extends AppCompatActivity {
         setContentView(R.layout.activity_second);
         btn1 = findViewById(R.id.imageButton);
         btn2 = findViewById(R.id.imageButton2);
+        tv = findViewById(R.id.textView);
+        res = findViewById(R.id.textView2);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ActivitySecond.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        if(getIntent() != null && getIntent().hasExtra(Intent.EXTRA_TEXT)){
+            tv.setText(getIntent().getStringExtra(Intent.EXTRA_TEXT));
+        }
 
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String result = res.getText().toString();
+                Intent intent = new Intent(ActivitySecond.this, MainActivity.class);
+                intent.putExtra(Intent.EXTRA_TEXT,result);
+                startActivity(intent);
             }
         });
 
--- ActivitySwitcher/app/src/main/java/com/addy/activityswitcher/MainActivity.java
+++ ActivitySwitcher/app/src/main/java/com/addy/activityswitcher/MainActivity.java
@@ -5,21 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    ImageButton btn;
+    ImageButton btn,btnSend;
+    EditText et1;
+    TextView v;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.imageBtn1);
+        et1 = findViewById(R.id.editText);
+        btnSend =findViewById(R.id.imageBtn);
+        v = findViewById(R.id.textView2);
+
+        if(getIntent() != null && getIntent().hasExtra(Intent.EXTRA_TEXT)){
+            v.setText(getIntent().getStringExtra(Intent.EXTRA_TEXT));
+        }
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,ActivitySecond.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = et1.getText().toString();
+                Intent intent = new Intent(MainActivity.this,ActivitySecond.class);
+                intent.putExtra(intent.EXTRA_TEXT,text);
                 startActivity(intent);
             }
         });

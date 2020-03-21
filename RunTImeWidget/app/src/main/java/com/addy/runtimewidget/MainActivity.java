934b2a9abb1aff3a5ae22bd5ba5fb1d7e279d357
Aditya Raj <aditya.x510@gmail.com>
2020-03-21 12:08:59 +0530

    init

--- RunTImeWidget/app/src/main/java/com/addy/runtimewidget/MainActivity.java
+++ RunTImeWidget/app/src/main/java/com/addy/runtimewidget/MainActivity.java
@@ -1,26 +1,43 @@
 package com.addy.runtimewidget;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.LinearLayout;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
-    LinearLayout ll;
+    Button btnDr,btnIn;
+    CircleImageView imageView;
+    ConstraintLayout cl;
+    String url1 = "https://cdn.pixabay.com/photo/2016/09/25/15/11/android-1693894_1280.jpg";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        btnDr = findViewById(R.id.btnDrawable);
+        btnIn = findViewById(R.id.btnInternet);
+        imageView = findViewById(R.id.loadImg);
+        cl = findViewById(R.id.clayout);
 
-        ll = findViewById(R.id.llayout);
-        ll = new LinearLayout(this);
-        DatePicker datePicker = new DatePicker(this);
-        ll.addView(datePicker);
-        Button btn = new Button(this);
-        btn.setText("Test");
-        ll.addView(btn);
-        setContentView(ll);
+        btnDr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cl.setVisibility(View.VISIBLE);
+                Picasso.get().load(R.drawable.mirageupgrade).into(imageView);
+            }
+        });
+
+        btnIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cl.setVisibility(View.VISIBLE);
+                Picasso.get().load(url1).into(imageView);
+            }
+        });
     }
 }
--- TestingMaps/app/src/main/java/com/addy/testingmaps/MapsActivity.java
+++ TestingMaps/app/src/main/java/com/addy/testingmaps/MapsActivity.java
@@ -1,14 +1,14 @@
 package com.addy.testingmaps;
 
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
-
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,7 +23,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private LocationListener locationListener;
     private LocationManager locationManager;
 
-    private final long MIN_TIME = 1000; // 1 second
+    private final long MIN_TIME = 100; // 1 second
     private final long MIN_DIST = 5; // 5 Meters
 
     private LatLng latLng;
@@ -32,7 +32,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-// Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -41,23 +41,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
 
     }
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-// Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title(""));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
         locationListener = new LocationListener() {
             @Override
@@ -65,13 +52,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 try {
                     latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                    mMap.addMarker(new MarkerOptions().position(latLng).title("My pos"));
+                    mMap.addMarker(new MarkerOptions().position(latLng).title("My location is :- " + latLng.toString()));
                     mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
                 }
                 catch (SecurityException e){
                     e.printStackTrace();
                 }
-
             }
 
             @Override
